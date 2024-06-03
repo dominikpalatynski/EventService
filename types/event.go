cdf059f422f184cb8d9edcbf28eaeb0f4c680726
dominikpalatynski <palatynski.dominik@gmail.com>
2024-06-03 12:48:32 +0200

    TimePeriod Added to Event Type

--- types/event.go
+++ types/event.go
@@ -1,8 +1,6 @@
 package types
 
 import (
-	"time"
-
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -11,15 +9,12 @@ type Event struct {
 	UserId string `json:"user_id" bson:"user_id" binding:"required"`
 	Title string `json:"title" binding:"required"`
 	ContentId primitive.ObjectID `json:"content,omitempty" bson:"content,omitempty"`
+	StartDate string `json:"start_date" bson:"start_date" binding:"required"`
+	EndDate   string `json:"end_date" bson:"end_date" binding:"required"`
 }
 
 type Content struct {
 	ID primitive.ObjectID 		`json:"id,omitempty" bson:"_id,omitempty"`
 	Homework string             `json:"homework,omitempty" bson:"homework,omitempty"`
 	Note string             `json:"note,omitempty" bson:"note,omitempty"`
-}
-
-type TimePeriod struct {
-	StartDate time.Time
-	EndDate time.Time
 }
