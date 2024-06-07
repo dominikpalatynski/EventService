ff815af7286fb0f9057e98df09402d81caf7f432
dominikpalatynski <palatynski.dominik@gmail.com>
2024-06-07 17:42:47 +0200

    Delaying messages

--- EventService/queue/queue.go
+++ EventService/queue/queue.go
@@ -23,6 +23,11 @@ type QueueHandler struct {
 	storage *storage.MongoDbStorage
 }
 
+type QueueContext struct {
+	connection *amqp.Connection
+	channel *amqp.Channel
+}
+
 func NewQueueHandler(s *storage.MongoDbStorage) *QueueHandler{
 	server := &QueueHandler{
 		storage: s,
@@ -35,12 +40,27 @@ func (q *QueueHandler) StartMonitor() {
 
 	defer ticker.Stop()
 
-	conn, err := amqp.Dial("amqp://michael:secret@localhost:5672")
+	conn, err := amqp.Dial("amqp://admin:password@localhost:5672")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
+
+	args := amqp.Table{
+		"x-delayed-type": "direct",
+	}
+	err = ch.ExchangeDeclare(
+		"delayed_exchange",     // name
+		"x-delayed-message",    // type
+		true,                   // durable
+		false,                  // auto-deleted
+		false,                  // internal
+		false,                  // no-wait
+		args,                   // arguments
+	)
+	failOnError(err, "Failed to declare the delayed exchange")
+
 	queue, err := ch.QueueDeclare(
 		"hello",
 		false,  
@@ -48,8 +68,18 @@ func (q *QueueHandler) StartMonitor() {
 		false,   
 		false,  
 		nil,)
+	
 	  failOnError(err, "Failed to declare a queue")
-	  
+
+	err = ch.QueueBind(
+		queue.Name,
+		"delayed_key",
+		"delayed_exchange",
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to bind the queue")
+
 	  ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	  defer cancel()
 	  
@@ -61,15 +91,22 @@ func (q *QueueHandler) StartMonitor() {
 	for range ticker.C {
 		currentTime := time.Now()
 		fmt.Println("fetching events:", currentTime)
+
+		delay := int64(15 * 1000) // 2 minuty w milisekundach; możesz zmienić na dynamiczną wartość
+
 		err = ch.PublishWithContext(ctx,
-			"",     // exchange
-			queue.Name, // routing key
-			false,  // mandatory
-			false,  // immediate
-			amqp.Publishing {
-			  ContentType: "text/plain",
-			  Body:        []byte(body),
+			"delayed_exchange", // exchange
+			"delayed_key",      // routing key
+			false,              // mandatory
+			false,              // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+				Headers: amqp.Table{
+					"x-delay": delay,
+				},
 			})
+		failOnError(err, "Failed to publish a delayed message")
 		twoMinutesLater := currentTime.Add(2 * time.Minute)
 
 
